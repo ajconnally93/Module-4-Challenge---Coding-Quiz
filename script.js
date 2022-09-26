7f4738e47ffa0c76a6e5f6e54e5f10d6f1bbd3fd
Andrew Connally <ajconnally93@gmail.com>
2022-09-26 11:48:04 -0500

    Committing changes so askBCS can see my Repo for help. Added comments describing every issue and functionality in my JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-// both are giving error
-var startQuizButton = document.getElementById("startButton");
-var quizQuestionsList = document.getElementById("quizQuestions");
+// both are HTML elements, startButton is a Button, quizQuestions is a Main
 
+// <button id="startButton">Click Here to Begin</button>
+// <main id="quizQuestions">
+    // <section>Question 1: should you</section>
+// </main>
+var startButton = document.getElementById("startButton");
+var quizQuestions = document.getElementById("quizQuestions");
+
+// properly displays 'TEST TEST TEST TEST TEST' to console
 console.log("TEST TEST TEST TEST TEST");
 
-var startQuiz = function () {
-    // display quiz
+// returns 'Null' in console
+console.log(startButton);
+
+function startQuiz () {
     console.log("ABCDEFGHIJKLMNOP");
-    quizQuestionsList.style.display = block;
+    quizQuestions.style.display = block;
 }
 
-startQuizButton.addEventListener('click', startQuiz);
+// Can call function and 'ABCDEFGHIJKLMNOP' will properly log to console. However, quizQuestions doesn't change to block Display
+startQuiz ();
+
+// Returns error in console: 
+// Uncaught TypeError: Cannot read properties of null (reading 'style')
+    // at startQuiz (script.js:18:19)
+    // at script.js:22:1
+startButton.addEventListener('click', startQuiz);
