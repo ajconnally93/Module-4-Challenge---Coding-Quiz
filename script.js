c5f769fb6bb8df8aa8b9e889874d0ca25b47798d
Andrew Connally <ajconnally93@gmail.com>
2022-09-25 23:15:42 -0500

    Commit before going to bed - 9/25 - Added some basic JS, Event Listener still not working correctly, but can get the console to log on global scope

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var startButton = getElementById("startButton");
+// both are giving error
+var startQuizButton = document.getElementById("startButton");
+var quizQuestionsList = document.getElementById("quizQuestions");
 
-startButton = function () {
+console.log("TEST TEST TEST TEST TEST");
+
+var startQuiz = function () {
     // display quiz
+    console.log("ABCDEFGHIJKLMNOP");
+    quizQuestionsList.style.display = block;
 }
 
-addEventListener(click, startButton) {
-
-}
+startQuizButton.addEventListener('click', startQuiz);
